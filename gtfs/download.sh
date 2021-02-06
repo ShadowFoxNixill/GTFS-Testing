0202cfca5622883ca01ad5d85d63e70012ade7ed
ShadowFoxNixill <github@nix.nixill.net>
2021-02-06 01:12:50 -0500

    Initial commit

---
+++ gtfs/download.sh
@@ -0,0 +1,3 @@
+curl -o ddot_gtfs.zip "https://data.detroitmi.gov/api/views/y62d-bvsz/files/7b0850af-3a25-46d7-84ee-fbc75a9dff0f?filename=ddot_gtfs.zip"
+curl -o annarbor_gtfs.zip "https://www.theride.org/sites/default/files/google/google_transit.zip"
+curl -o windsor_gtfs.zip "https://opendata.citywindsor.ca/Uploads/google_transit.zip"
---
+++ gtfs/readme.md
@@ -0,0 +1,3 @@
+Run `download.sh` to obtain GTFS files for testing.
+
+Files are provided by transit sytems from the greater Detroit, MI area and subject to separate license agreements.
